703826e16260fa94bcd1e0528d148639c32061e4
coreybyrum7 <coreybyrum7@gmail.com>
2017-06-27 12:54:58 -0400

    Completed Goal Coach

--- goalcoach/src/components/App.jsx
+++ goalcoach/src/components/App.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firebaseApp } from '../firebase';
+import AddGoal from './AddGoal';
+import GoalList from './GoalList';
+import CompleteGoalList from './CompleteGoalList'
 
 class App extends Component {
+    
+    signOut() {
+        firebaseApp.auth().signOut();
+    }
+    
     render() {
         return (
-            <div>App</div>
+            <div style={{margin: '5px'}}>
+                <h3>Goal Coach</h3>
+                <AddGoal />
+                <hr />
+                <h4>Goals</h4>
+                <GoalList />
+                <hr />
+                <h4>Completed Goals</h4>
+                <CompleteGoalList />
+                <hr />
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.signOut()}
+                >
+                    Sign Out
+                </button>
+            </div>
         )
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {}
+}
+
+export default connect(mapStateToProps, null)(App);
